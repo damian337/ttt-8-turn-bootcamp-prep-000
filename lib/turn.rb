95a8418922da9ec263599a99c99db5ef49e90c4f
Damian Guillory <guillorywdamian@gmail.com>
2017-10-22 23:41:45 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,25 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+    if index.between?(0, 8) && position_taken?(board, index) == false
+        true
+    end
+end
+
+def position_taken?(board, index)
+    if board[index] == " "
+        false
+    elsif board[index] == ""
+        false
+    elsif board[index] == nil
+        false
+    else
+        true
+    end
+end
